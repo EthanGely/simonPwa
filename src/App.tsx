22b5ec6cda3d2ea477f05401a2adcfb872774057
Ethan <ethangely@free.fr>
2024-02-14 11:31:31 +0100

    working click on color

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
-import { useState } from 'react'
+import {useCallback, useState} from 'react'
 import './App.css'
 
 function App() {
-  const [sequence, setSequence] = useState(new Array(50));
-  const [currentIndex, setCurrentIndex] = useState(-1);
-  const [isPlayerTurn, setIsPlayerTurn] = useState(false);
+    const [sequence, setSequence] = useState(new Array(50));
+    const [currentIndex, setCurrentIndex] = useState(-1);
+    const [isPlayerTurn, setIsPlayerTurn] = useState(true);
 
-  return (
-    <>
-      <div className="playField">
-          <div className="red hoverable"></div>
-          <div className="blue hoverable"></div>
-          <div className="green hoverable"></div>
-          <div className="yellow hoverable"></div>
-      </div>
-    </>
-  )
+    const handleClick = useCallback((color) => {
+        if (isPlayerTurn) {
+            console.log(color);
+            setIsPlayerTurn(!isPlayerTurn)
+        } else {
+            console.log(color);
+            setIsPlayerTurn(!isPlayerTurn)
+        }
+    }, [isPlayerTurn]);
+
+    return (
+        <>
+            <div className="playField">
+                <div id="red" className="red hoverable" onClick={() => handleClick("red")}></div>
+                <div id="blue" className="blue hoverable" onClick={() => handleClick("blue")}></div>
+                <div id="green" className="green hoverable" onClick={() => handleClick("green")}></div>
+                <div id="yellow" className="yellow hoverable" onClick={() => handleClick("yellow")}></div>
+            </div>
+        </>
+    )
 }
 
 export default App
